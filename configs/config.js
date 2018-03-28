6cd7e90441b5bb9227c9e4706e63b1eb8bbfcbe5
You <you85630@foxmail.com>
2018-03-28 18:48:58 +0800

    init

--- configs/config.js
+++ configs/config.js
@@ -75,11 +75,11 @@ const config = {
     /**
      * Source Maps
      */
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
-    cssSourceMap: true,
-    productionSourceMap: true
+    cssSourceMap: false,
+    productionSourceMap: false
   },
   nodeConfiguration: {
     global: false,
